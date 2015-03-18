3f5e10f73681bc9b2b5ddb5d1ab0cf05ecc50cbe
Potteplanten <bi-z@hotmail.com>
2015-03-18 12:03:10 +0100

    Innlevering sql

--- SykkelMaster/sider/innlevering.vb
+++ SykkelMaster/sider/innlevering.vb
@@ -6,4 +6,13 @@
 
         DataGridView1.DataSource = payload
     End Sub
+
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        Dim payload As New DataTable
+        Dim sqlNavnCombo As String = "SELECT fornavn, etternavn, telefon FROM person WHERE fornavn LIKE '%" & TextBox3.Text & "%'  "
+        payload = db.query(sqlNavnCombo)
+
+
+    End Sub
 End Class
