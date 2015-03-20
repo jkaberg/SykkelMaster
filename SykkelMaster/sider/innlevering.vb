85400e8457628152ecf4f843ce91f23fcba8ed89
Potteplanten <bi-z@hotmail.com>
2015-03-20 13:16:01 +0100

    Comboboxer innleveringer

--- SykkelMaster/sider/innlevering.vb
+++ SykkelMaster/sider/innlevering.vb
@@ -9,27 +9,51 @@ Public Class innlevering
     End Sub
 
     Public telefon As Integer
+
+
     Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
         Dim payload As New DataTable
         Dim rad As DataRow
-        Dim sqlNavnCombo As String = "SELECT fornavn, etternavn, telefon FROM person WHERE fornavn LIKE '" & TextBox3.Text & "%' OR etternavn LIKE '" & TextBox3.Text & "%' OR telefon LIKE '" & TextBox3.Text & "%' "
-        payload = db.query(sqlNavnCombo)
-        Dim fornavn, etternavn As String
+        Dim sqlNavnCombo As String = "SELECT * FROM person WHERE fornavn LIKE '" & TextBox3.Text & "%' OR etternavn LIKE '" & TextBox3.Text & "%' OR telefon LIKE '" & TextBox3.Text & "%' "
+        If TextBox3.Text <> "" Then
+            payload = db.query(sqlNavnCombo)
+        End If
 
+        Dim fornavn, etternavn As String
+        Dim id As Integer
         ComboBox1.Items.Clear()
         'Kunder blir lagt ut i ComboBox
         For Each rad In payload.Rows
             fornavn = rad("fornavn")
             etternavn = rad("etternavn")
             telefon = rad("telefon")
-            ComboBox1.Items.Add(fornavn & " " & etternavn)
+            id = rad("id")
+            ComboBox1.Items.Add(id & " " & fornavn & " " & etternavn)
+            ComboBox1.SelectedIndex = ComboBox1.FindString(rad.Item(id))
 
         Next
 
     End Sub
 
+
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
         'Telefon blir vist for vertifisering av kunde
         TextBox2.Text = telefon
+        Dim payload As New DataTable
+        Dim rad As DataRow
+        Dim sqlOrdreCombo As String = "SELECT * FROM salg_leie, person WHERE salg_leie.person_id_kunde = person.id AND person.id = '" & ComboBox1.SelectedItem() & "'"
+        Dim ordrenr As Integer
+        payload = db.query(sqlOrdreCombo)
+
+        ComboBox2.Items.Clear()
+        For Each rad In payload.Rows
+
+            ordrenr = rad("ordre_nr")
+            ComboBox2.Items.Add(ordrenr)
+        Next
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
     End Sub
 End Class
