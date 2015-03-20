161a10efc32ec8951cd3e6d091c66564715c01d0
jensron <jensronne@hotmail.com>
2015-03-20 11:15:15 +0100

    kunde
    GridView

--- SykkelMaster/sider/kunder.Designer.vb
+++ SykkelMaster/sider/kunder.Designer.vb
@@ -26,27 +26,27 @@ Partial Class kunder
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.txtPoststed = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.txtMail = New System.Windows.Forms.TextBox()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.txtPostnr = New System.Windows.Forms.TextBox()
+        Me.txtAdresse = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txttelefon = New System.Windows.Forms.TextBox()
+        Me.txtEtternavn = New System.Windows.Forms.TextBox()
+        Me.txtNavn = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.brukerGridView = New System.Windows.Forms.DataGridView()
+        Me.sokKunde = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.brukerGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -54,41 +54,45 @@ Partial Class kunder
         Me.GroupBox1.Controls.Add(Me.Button5)
         Me.GroupBox1.Controls.Add(Me.Button3)
         Me.GroupBox1.Controls.Add(Me.Label8)
-        Me.GroupBox1.Controls.Add(Me.TextBox8)
+        Me.GroupBox1.Controls.Add(Me.txtPoststed)
         Me.GroupBox1.Controls.Add(Me.Label7)
-        Me.GroupBox1.Controls.Add(Me.TextBox7)
+        Me.GroupBox1.Controls.Add(Me.txtMail)
         Me.GroupBox1.Controls.Add(Me.Button4)
         Me.GroupBox1.Controls.Add(Me.Label5)
-        Me.GroupBox1.Controls.Add(Me.TextBox5)
-        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.txtPostnr)
+        Me.GroupBox1.Controls.Add(Me.txtAdresse)
         Me.GroupBox1.Controls.Add(Me.Label4)
         Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.TextBox3)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.txttelefon)
+        Me.GroupBox1.Controls.Add(Me.txtEtternavn)
+        Me.GroupBox1.Controls.Add(Me.txtNavn)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 16)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(264, 325)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(297, 406)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Ny/rediger kunde"
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(6, 286)
+        Me.Button5.Location = New System.Drawing.Point(7, 358)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(240, 30)
+        Me.Button5.Size = New System.Drawing.Size(270, 38)
         Me.Button5.TabIndex = 30
         Me.Button5.Text = "Slett"
         Me.Button5.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(6, 250)
+        Me.Button3.Location = New System.Drawing.Point(7, 312)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(240, 30)
+        Me.Button3.Size = New System.Drawing.Size(270, 38)
         Me.Button3.TabIndex = 29
         Me.Button3.Text = "Oppdater"
         Me.Button3.UseVisualStyleBackColor = True
@@ -96,40 +100,43 @@ Partial Class kunder
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(6, 189)
+        Me.Label8.Location = New System.Drawing.Point(7, 236)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(63, 17)
+        Me.Label8.Size = New System.Drawing.Size(72, 20)
         Me.Label8.TabIndex = 28
         Me.Label8.Text = "Poststed"
         '
-        'TextBox8
+        'txtPoststed
         '
-        Me.TextBox8.Location = New System.Drawing.Point(80, 186)
-        Me.TextBox8.Name = "TextBox8"
-        Me.TextBox8.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox8.TabIndex = 27
+        Me.txtPoststed.Location = New System.Drawing.Point(90, 232)
+        Me.txtPoststed.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPoststed.Name = "txtPoststed"
+        Me.txtPoststed.Size = New System.Drawing.Size(186, 26)
+        Me.txtPoststed.TabIndex = 27
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(6, 105)
+        Me.Label7.Location = New System.Drawing.Point(7, 131)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(49, 17)
+        Me.Label7.Size = New System.Drawing.Size(56, 20)
         Me.Label7.TabIndex = 26
         Me.Label7.Text = "E-post"
         '
-        'TextBox7
+        'txtMail
         '
-        Me.TextBox7.Location = New System.Drawing.Point(80, 102)
-        Me.TextBox7.Name = "TextBox7"
-        Me.TextBox7.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox7.TabIndex = 25
+        Me.txtMail.Location = New System.Drawing.Point(90, 128)
+        Me.txtMail.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtMail.Name = "txtMail"
+        Me.txtMail.Size = New System.Drawing.Size(186, 26)
+        Me.txtMail.TabIndex = 25
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(6, 214)
+        Me.Button4.Location = New System.Drawing.Point(7, 268)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(240, 30)
+        Me.Button4.Size = New System.Drawing.Size(270, 38)
         Me.Button4.TabIndex = 22
         Me.Button4.Text = "Legg til"
         Me.Button4.UseVisualStyleBackColor = True
@@ -137,128 +144,138 @@ Partial Class kunder
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 161)
+        Me.Label5.Location = New System.Drawing.Point(7, 201)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(49, 17)
+        Me.Label5.Size = New System.Drawing.Size(55, 20)
         Me.Label5.TabIndex = 10
         Me.Label5.Text = "Postnr"
         '
-        'TextBox5
+        'txtPostnr
         '
-        Me.TextBox5.Location = New System.Drawing.Point(80, 158)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox5.TabIndex = 9
+        Me.txtPostnr.Location = New System.Drawing.Point(90, 198)
+        Me.txtPostnr.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPostnr.Name = "txtPostnr"
+        Me.txtPostnr.Size = New System.Drawing.Size(186, 26)
+        Me.txtPostnr.TabIndex = 9
         '
-        'TextBox4
+        'txtAdresse
         '
-        Me.TextBox4.Location = New System.Drawing.Point(80, 130)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox4.TabIndex = 8
+        Me.txtAdresse.Location = New System.Drawing.Point(90, 162)
+        Me.txtAdresse.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtAdresse.Name = "txtAdresse"
+        Me.txtAdresse.Size = New System.Drawing.Size(186, 26)
+        Me.txtAdresse.TabIndex = 8
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 133)
+        Me.Label4.Location = New System.Drawing.Point(7, 166)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(60, 17)
+        Me.Label4.Size = New System.Drawing.Size(68, 20)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "Adresse"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 77)
+        Me.Label3.Location = New System.Drawing.Point(7, 96)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(69, 17)
+        Me.Label3.Size = New System.Drawing.Size(76, 20)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Telefonnr"
         '
-        'TextBox3
+        'txttelefon
         '
-        Me.TextBox3.Location = New System.Drawing.Point(80, 74)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox3.TabIndex = 4
+        Me.txttelefon.Location = New System.Drawing.Point(90, 92)
+        Me.txttelefon.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txttelefon.Name = "txttelefon"
+        Me.txttelefon.Size = New System.Drawing.Size(186, 26)
+        Me.txttelefon.TabIndex = 4
         '
-        'TextBox2
+        'txtEtternavn
         '
-        Me.TextBox2.Location = New System.Drawing.Point(80, 46)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox2.TabIndex = 3
+        Me.txtEtternavn.Location = New System.Drawing.Point(90, 58)
+        Me.txtEtternavn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtEtternavn.Name = "txtEtternavn"
+        Me.txtEtternavn.Size = New System.Drawing.Size(186, 26)
+        Me.txtEtternavn.TabIndex = 3
         '
-        'TextBox1
+        'txtNavn
         '
-        Me.TextBox1.Location = New System.Drawing.Point(80, 18)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(166, 22)
-        Me.TextBox1.TabIndex = 2
+        Me.txtNavn.Location = New System.Drawing.Point(90, 22)
+        Me.txtNavn.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtNavn.Name = "txtNavn"
+        Me.txtNavn.Size = New System.Drawing.Size(186, 26)
+        Me.txtNavn.TabIndex = 2
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(6, 49)
+        Me.Label2.Location = New System.Drawing.Point(7, 61)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(69, 17)
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Etternavn"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 21)
+        Me.Label1.Location = New System.Drawing.Point(7, 26)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(60, 17)
+        Me.Label1.Size = New System.Drawing.Size(67, 20)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Fornavn"
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.DataGridView1)
-        Me.GroupBox2.Controls.Add(Me.TextBox6)
+        Me.GroupBox2.Controls.Add(Me.brukerGridView)
+        Me.GroupBox2.Controls.Add(Me.sokKunde)
         Me.GroupBox2.Controls.Add(Me.Label6)
-        Me.GroupBox2.Location = New System.Drawing.Point(284, 13)
+        Me.GroupBox2.Location = New System.Drawing.Point(320, 16)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(447, 325)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(503, 406)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Søk"
         '
-        'DataGridView1
+        'brukerGridView
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(11, 46)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(432, 270)
-        Me.DataGridView1.TabIndex = 18
+        Me.brukerGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.brukerGridView.Location = New System.Drawing.Point(12, 58)
+        Me.brukerGridView.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.brukerGridView.Name = "brukerGridView"
+        Me.brukerGridView.RowTemplate.Height = 24
+        Me.brukerGridView.Size = New System.Drawing.Size(486, 338)
+        Me.brukerGridView.TabIndex = 18
         '
-        'TextBox6
+        'sokKunde
         '
-        Me.TextBox6.Location = New System.Drawing.Point(113, 18)
-        Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(328, 22)
-        Me.TextBox6.TabIndex = 16
+        Me.sokKunde.Location = New System.Drawing.Point(127, 22)
+        Me.sokKunde.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.sokKunde.Name = "sokKunde"
+        Me.sokKunde.Size = New System.Drawing.Size(368, 26)
+        Me.sokKunde.TabIndex = 16
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 21)
+        Me.Label6.Location = New System.Drawing.Point(7, 26)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(101, 17)
+        Me.Label6.Size = New System.Drawing.Size(112, 20)
         Me.Label6.TabIndex = 17
         Me.Label6.Text = "Navn/telefonnr"
         '
         'kunder
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(739, 343)
+        Me.ClientSize = New System.Drawing.Size(831, 429)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "kunder"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Kunde"
@@ -266,30 +283,30 @@ Partial Class kunder
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.brukerGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAdresse As System.Windows.Forms.TextBox
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txttelefon As System.Windows.Forms.TextBox
+    Friend WithEvents txtEtternavn As System.Windows.Forms.TextBox
+    Friend WithEvents txtNavn As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPostnr As System.Windows.Forms.TextBox
     Friend WithEvents Button4 As System.Windows.Forms.Button
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents brukerGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents sokKunde As System.Windows.Forms.TextBox
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents txtMail As System.Windows.Forms.TextBox
     Friend WithEvents Button5 As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPoststed As System.Windows.Forms.TextBox
 End Class
--- SykkelMaster/sider/kunder.vb
+++ SykkelMaster/sider/kunder.vb
@@ -1,9 +1,41 @@
 ﻿Public Class kunder
+    Private gridIndex As Integer
 
     Private Sub kunder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim payload As New DataTable
-        payload = db.query("SELECT * FROM person WHERE person.person_id NOT IN (SELECT person_id FROM ansatt)")
+        payload = db.query("SELECT * FROM person WHERE person.id")
 
-        DataGridView1.DataSource = payload
+        brukerGridView.DataSource = payload
     End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles brukerGridView.CellClick
+        'Sett nuvarende DataGrid index sånn at vi finner rett rad i DataTablen
+        gridIndex = brukerGridView.CurrentRow.Index
+
+        'Sett in dataen fra Grid Viewn i Textboksene
+        With Me.brukerGridView
+            txtNavn.Text = .Rows(gridIndex).Cells("fornavn").Value
+            txtEtternavn.Text = .Rows(gridIndex).Cells("etternavn").Value
+            txttelefon.Text = .Rows(gridIndex).Cells("telefon").Value
+            txtAdresse.Text = .Rows(gridIndex).Cells("adresse").Value
+            txtMail.Text = .Rows(gridIndex).Cells("mail").Value
+            txtPostnr.Text = .Rows(gridIndex).Cells("post_nr").Value
+
+        End With
+    End Sub
+
+    Private Sub sokKunde_TextChanged(sender As Object, e As EventArgs) Handles sokKunde.TextChanged
+
+    End Sub
+
+    Private Sub oppdaterGridView(Optional ByVal sok As String = Nothing)
+        Dim payload As New DataTable
+        Dim sql As String
+        If sok Then
+            sql = "SELECT * FROM person LIKE %_"
+        Else
+
+        End If
+    End Sub
+
 End Class
