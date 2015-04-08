5cf71672be79cf4111784589cda9d9a0d3305c41
Joel Kåberg <joel@jkaberg.com>
2015-04-08 17:55:08 +0200

    statistikk - bedre kommentarer

--- SykkelMaster/sider/statistikk.vb
+++ SykkelMaster/sider/statistikk.vb
@@ -1,33 +1,34 @@
 ﻿Public Class statistikk
     Private payload As DataTable
-    ' Antal sykkler per sykkeltype
-    ' Mest populære sykklene
-    ' Hvor mange sykkler per "lagerplass"
-    ' Hvilke sykkler blir mest ødelagd
 
     Private Sub cbxStats_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxStats.SelectedIndexChanged
         Dim sql As String
+
         ' Rens ut nuvarende informasjon som evt er i grafen
         graf.Series(0).Points.Clear()
         graf.DataSource = ""
 
         Select Case cbxStats.SelectedIndex
             Case 0
+                ' Antal sykkler per sykkeltype - FERDIG
                 sql = "SELECT sykkeltype.sykkeltype AS navn, count(*) AS num " &
                       "FROM sykkel " &
                       "JOIN sykkeltype ON sykkeltype.id = sykkel.sykkeltype " &
                       "GROUP BY sykkeltype.sykkeltype;"
             Case 1
+                ' Mest populære sykklene - IKKE FERDIG
                 sql = "SELECT sykkeltype.sykkeltype AS navn, count(*) AS num " &
                       "FROM sykkel " &
                       "JOIN sykkeltype ON sykkeltype.id = sykkel.sykkeltype " &
                       "GROUP BY sykkeltype.sykkeltype;"
             Case 2
+                ' Hvor mange sykkler per "lagerplass" - IKKE FERDIG
                 sql = "SELECT sykkeltype.sykkeltype AS navn, count(*) AS num " &
                       "FROM sykkel " &
                       "JOIN sykkeltype ON sykkeltype.id = sykkel.sykkeltype " &
                       "GROUP BY sykkeltype.sykkeltype;"
             Case 3
+                ' Hvilke sykkler blir mest ødelagd - IKKE FERDIG
                 sql = "SELECT sykkeltype.sykkeltype AS navn, count(*) AS num " &
                       "FROM sykkel " &
                       "JOIN sykkeltype ON sykkeltype.id = sykkel.sykkeltype " &
