b5360ba8cd097c0bdb95affd474b3db0a14a85c0
Joel Kåberg <joel@jkaberg.com>
2015-04-09 16:26:31 +0200

    statistikk - fikset 3d

--- SykkelMaster/sider/statistikk.Designer.vb
+++ SykkelMaster/sider/statistikk.Designer.vb
@@ -22,9 +22,9 @@ Partial Class statistikk
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ChartArea4 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend4 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series4 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.graf = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.cbxStats = New System.Windows.Forms.ComboBox()
         CType(Me.graf, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -35,52 +35,52 @@ Partial Class statistikk
         Me.graf.BackColor = System.Drawing.Color.Transparent
         Me.graf.BorderlineColor = System.Drawing.Color.Transparent
         Me.graf.BorderlineWidth = 0
-        ChartArea4.Area3DStyle.Enable3D = True
-        ChartArea4.Area3DStyle.IsClustered = True
-        ChartArea4.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic
-        ChartArea4.Area3DStyle.PointDepth = 30
-        ChartArea4.Area3DStyle.PointGapDepth = 30
-        ChartArea4.Area3DStyle.WallWidth = 1
-        ChartArea4.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight
-        ChartArea4.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.BackwardDiagonal
-        ChartArea4.BackImageTransparentColor = System.Drawing.Color.Transparent
-        ChartArea4.CursorY.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet
-        ChartArea4.Name = "ChartArea1"
-        ChartArea4.ShadowColor = System.Drawing.Color.DarkGray
-        Me.graf.ChartAreas.Add(ChartArea4)
-        Legend4.Alignment = System.Drawing.StringAlignment.Far
-        Legend4.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet
-        Legend4.Enabled = False
-        Legend4.HeaderSeparator = System.Windows.Forms.DataVisualization.Charting.LegendSeparatorStyle.Line
-        Legend4.HeaderSeparatorColor = System.Drawing.Color.Transparent
-        Legend4.InterlacedRows = True
-        Legend4.IsEquallySpacedItems = True
-        Legend4.ItemColumnSeparatorColor = System.Drawing.Color.Transparent
-        Legend4.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column
-        Legend4.Name = "Legend1"
-        Legend4.TableStyle = System.Windows.Forms.DataVisualization.Charting.LegendTableStyle.Wide
-        Legend4.TitleBackColor = System.Drawing.Color.Black
-        Me.graf.Legends.Add(Legend4)
+        ChartArea1.Area3DStyle.Enable3D = True
+        ChartArea1.Area3DStyle.IsClustered = True
+        ChartArea1.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic
+        ChartArea1.Area3DStyle.PointDepth = 30
+        ChartArea1.Area3DStyle.PointGapDepth = 30
+        ChartArea1.Area3DStyle.WallWidth = 1
+        ChartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight
+        ChartArea1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.BackwardDiagonal
+        ChartArea1.BackImageTransparentColor = System.Drawing.Color.Transparent
+        ChartArea1.CursorY.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet
+        ChartArea1.Name = "ChartArea1"
+        ChartArea1.ShadowColor = System.Drawing.Color.DarkGray
+        Me.graf.ChartAreas.Add(ChartArea1)
+        Legend1.Alignment = System.Drawing.StringAlignment.Far
+        Legend1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet
+        Legend1.Enabled = False
+        Legend1.HeaderSeparator = System.Windows.Forms.DataVisualization.Charting.LegendSeparatorStyle.Line
+        Legend1.HeaderSeparatorColor = System.Drawing.Color.Transparent
+        Legend1.InterlacedRows = True
+        Legend1.IsEquallySpacedItems = True
+        Legend1.ItemColumnSeparatorColor = System.Drawing.Color.Transparent
+        Legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column
+        Legend1.Name = "Legend1"
+        Legend1.TableStyle = System.Windows.Forms.DataVisualization.Charting.LegendTableStyle.Wide
+        Legend1.TitleBackColor = System.Drawing.Color.Black
+        Me.graf.Legends.Add(Legend1)
         Me.graf.Location = New System.Drawing.Point(12, 12)
         Me.graf.Name = "graf"
-        Series4.BackImageTransparentColor = System.Drawing.Color.Transparent
-        Series4.BackSecondaryColor = System.Drawing.Color.Transparent
-        Series4.BorderColor = System.Drawing.Color.Black
-        Series4.ChartArea = "ChartArea1"
-        Series4.Color = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Series4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
-        Series4.IsValueShownAsLabel = True
-        Series4.LabelBackColor = System.Drawing.Color.LightGray
-        Series4.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet
-        Series4.LabelBorderWidth = 0
-        Series4.Legend = "Legend1"
-        Series4.MarkerBorderColor = System.Drawing.Color.Transparent
-        Series4.MarkerColor = System.Drawing.Color.Transparent
-        Series4.Name = "Series1"
-        Series4.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent
-        Series4.ShadowColor = System.Drawing.Color.Beige
-        Series4.SmartLabelStyle.Enabled = False
-        Me.graf.Series.Add(Series4)
+        Series1.BackImageTransparentColor = System.Drawing.Color.Transparent
+        Series1.BackSecondaryColor = System.Drawing.Color.Transparent
+        Series1.BorderColor = System.Drawing.Color.Black
+        Series1.ChartArea = "ChartArea1"
+        Series1.Color = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Series1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Series1.IsValueShownAsLabel = True
+        Series1.LabelBackColor = System.Drawing.Color.LightGray
+        Series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet
+        Series1.LabelBorderWidth = 0
+        Series1.Legend = "Legend1"
+        Series1.MarkerBorderColor = System.Drawing.Color.Transparent
+        Series1.MarkerColor = System.Drawing.Color.Transparent
+        Series1.Name = "Series1"
+        Series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent
+        Series1.ShadowColor = System.Drawing.Color.Beige
+        Series1.SmartLabelStyle.Enabled = False
+        Me.graf.Series.Add(Series1)
         Me.graf.Size = New System.Drawing.Size(897, 467)
         Me.graf.TabIndex = 0
         Me.graf.Text = "Chart1"
