5d8e168e1a15f0312b849092b613d0dc924128a2
Joel Kåberg <joel@jkaberg.com>
2015-04-13 19:00:57 +0200

    start- rens

--- SykkelMaster/start.vb
+++ SykkelMaster/start.vb
@@ -19,18 +19,13 @@
     Private Sub Sendnyttpassord_click(sender As Object, e As EventArgs) Handles SendNyttPassord.Click
         Dim passord As String = verktoy.tilfeldigStreng()
 
-        If verktoy.validerEpost(byttEpost.Text) Then
-            If daoDelt.sjekkMailEksisterer(byttEpost.Text) Then
-                If daoStart.oppdaterPassord(passord, byttEpost.Text) Then
-                    verktoy.sendMail(byttEpost.Text, "Nytt Passord", "Her er ditt nye passord: " & passord)
-                    MsgBox("Ditt nye passord er sendt til deg på mail.", MsgBoxStyle.Information)
-                End If
-            Else
-                MsgBox("E-post adressen eksisterer ikke.", MsgBoxStyle.Critical)
+        If daoDelt.sjekkMailEksisterer(byttEpost.Text) Then
+            If daoStart.oppdaterPassord(passord, byttEpost.Text) Then
+                verktoy.sendMail(byttEpost.Text, "Nytt Passord", "Her er ditt nye passord: " & passord)
+                MsgBox("Ditt nye passord er sendt til deg på mail.", MsgBoxStyle.Information)
             End If
         Else
-            MsgBox("E-post adressen har ikke en korrekt formatering.", MsgBoxStyle.Critical)
+            MsgBox("E-post adressen eksisterer ikke.", MsgBoxStyle.Information)
         End If
-        byttEpost.Text = ""
     End Sub
 End Class
