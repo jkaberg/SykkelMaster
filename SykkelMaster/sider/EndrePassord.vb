0c41adae7650a4b114ea320080fd29ab1042a7ac
Joel Kåberg <joel@jkaberg.com>
2015-04-15 11:40:01 +0200

    fixes #22

--- SykkelMaster/sider/EndrePassord.vb
+++ SykkelMaster/sider/EndrePassord.vb
@@ -3,17 +3,13 @@
         'Knappen sjekker først input om tekstfeltene er under 30 karakterer
         'Så kjøres sjekkpassord om det finnes i databasen
         'Deretter kjører prosedyren endrepassord og endrer passordet. 
-        If daoEndrePassord.sjekkPassord(start.bruker.pEpost, nåværendePassordtxt.Text) Then
-            If nyttPassordtxt.Text.Length > 30 Or nåværendePassordtxt.Text.Length > 30 Then
-                MsgBox("Passord må inneholde mindre enn 30 tegn", MsgBoxStyle.Information)
-            Else
-                daoEndrePassord.endrePassord(nyttPassordtxt.Text, start.bruker.pEpost)
-                MsgBox("Passordet er endret til: " & nyttPassordtxt.Text, MsgBoxStyle.Information)
-                nåværendePassordtxt.Text = ""
-                nyttPassordtxt.Text = ""
-            End If
+        If nyttPassordtxt.Text.Length > 30 Or nåværendePassordtxt.Text.Length > 30 Then
+            MsgBox("Passord må inneholde mindre enn 30 tegn", MsgBoxStyle.Information)
         Else
-            MsgBox("Feil e-post/passord kombinasjon, eller så eksisterer brukern ikke.", MsgBoxStyle.Critical)
+            daoEndrePassord.endrePassord(nyttPassordtxt.Text, start.bruker.pEpost)
+            MsgBox("Passordet er endret til: " & nyttPassordtxt.Text, MsgBoxStyle.Information)
+            nåværendePassordtxt.Text = ""
+            nyttPassordtxt.Text = ""
         End If
     End Sub
 End Class
