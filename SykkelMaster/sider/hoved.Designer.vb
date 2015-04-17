01d26ed778167debecb5af7a4ebb1983dd404bad
Potteplanten <bi-z@hotmail.com>
2015-04-17 19:01:29 +0200

    same

--- SykkelMaster/sider/hoved.Designer.vb
+++ SykkelMaster/sider/hoved.Designer.vb
@@ -50,40 +50,40 @@ Partial Class hoved
         '
         'btnVis_Kunder
         '
-        Me.btnVis_Kunder.Location = New System.Drawing.Point(6, 73)
-        Me.btnVis_Kunder.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnVis_Kunder.Location = New System.Drawing.Point(4, 59)
+        Me.btnVis_Kunder.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnVis_Kunder.Name = "btnVis_Kunder"
-        Me.btnVis_Kunder.Size = New System.Drawing.Size(143, 49)
+        Me.btnVis_Kunder.Size = New System.Drawing.Size(107, 40)
         Me.btnVis_Kunder.TabIndex = 4
         Me.btnVis_Kunder.Text = "Kunder"
         Me.btnVis_Kunder.UseVisualStyleBackColor = True
         '
         'btnVis_Utleie
         '
-        Me.btnVis_Utleie.Location = New System.Drawing.Point(155, 20)
-        Me.btnVis_Utleie.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnVis_Utleie.Location = New System.Drawing.Point(116, 16)
+        Me.btnVis_Utleie.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnVis_Utleie.Name = "btnVis_Utleie"
-        Me.btnVis_Utleie.Size = New System.Drawing.Size(143, 49)
+        Me.btnVis_Utleie.Size = New System.Drawing.Size(107, 40)
         Me.btnVis_Utleie.TabIndex = 2
         Me.btnVis_Utleie.Text = "Utleie"
         Me.btnVis_Utleie.UseVisualStyleBackColor = True
         '
         'btnVis_Lokasjoner
         '
-        Me.btnVis_Lokasjoner.Location = New System.Drawing.Point(155, 20)
-        Me.btnVis_Lokasjoner.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnVis_Lokasjoner.Location = New System.Drawing.Point(116, 16)
+        Me.btnVis_Lokasjoner.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnVis_Lokasjoner.Name = "btnVis_Lokasjoner"
-        Me.btnVis_Lokasjoner.Size = New System.Drawing.Size(143, 49)
+        Me.btnVis_Lokasjoner.Size = New System.Drawing.Size(107, 40)
         Me.btnVis_Lokasjoner.TabIndex = 5
         Me.btnVis_Lokasjoner.Text = "Lokasjoner"
         Me.btnVis_Lokasjoner.UseVisualStyleBackColor = True
         '
         'btnVis_Sykler
         '
-        Me.btnVis_Sykler.Location = New System.Drawing.Point(6, 20)
-        Me.btnVis_Sykler.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnVis_Sykler.Location = New System.Drawing.Point(4, 16)
+        Me.btnVis_Sykler.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnVis_Sykler.Name = "btnVis_Sykler"
-        Me.btnVis_Sykler.Size = New System.Drawing.Size(143, 49)
+        Me.btnVis_Sykler.Size = New System.Drawing.Size(107, 40)
         Me.btnVis_Sykler.TabIndex = 3
         Me.btnVis_Sykler.Text = "Sykler"
         Me.btnVis_Sykler.UseVisualStyleBackColor = True
@@ -91,11 +91,11 @@ Partial Class hoved
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.cbxPlassering)
-        Me.GroupBox2.Location = New System.Drawing.Point(495, 12)
-        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox2.Location = New System.Drawing.Point(371, 10)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.GroupBox2.Size = New System.Drawing.Size(143, 57)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox2.Size = New System.Drawing.Size(107, 46)
         Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Lokasjon"
@@ -104,18 +104,18 @@ Partial Class hoved
         '
         Me.cbxPlassering.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbxPlassering.FormattingEnabled = True
-        Me.cbxPlassering.Location = New System.Drawing.Point(7, 22)
-        Me.cbxPlassering.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.cbxPlassering.Location = New System.Drawing.Point(5, 18)
+        Me.cbxPlassering.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cbxPlassering.Name = "cbxPlassering"
-        Me.cbxPlassering.Size = New System.Drawing.Size(121, 24)
+        Me.cbxPlassering.Size = New System.Drawing.Size(92, 21)
         Me.cbxPlassering.TabIndex = 0
         '
         'btnVis_Brukere
         '
-        Me.btnVis_Brukere.Location = New System.Drawing.Point(6, 20)
-        Me.btnVis_Brukere.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnVis_Brukere.Location = New System.Drawing.Point(4, 16)
+        Me.btnVis_Brukere.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnVis_Brukere.Name = "btnVis_Brukere"
-        Me.btnVis_Brukere.Size = New System.Drawing.Size(143, 49)
+        Me.btnVis_Brukere.Size = New System.Drawing.Size(107, 40)
         Me.btnVis_Brukere.TabIndex = 6
         Me.btnVis_Brukere.Text = "Brukere"
         Me.btnVis_Brukere.UseVisualStyleBackColor = True
@@ -123,11 +123,11 @@ Partial Class hoved
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.lblNavn)
-        Me.GroupBox1.Location = New System.Drawing.Point(13, 12)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox1.Location = New System.Drawing.Point(10, 10)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.GroupBox1.Size = New System.Drawing.Size(476, 57)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(357, 46)
         Me.GroupBox1.TabIndex = 6
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Informasjon"
@@ -135,58 +135,59 @@ Partial Class hoved
         'lblNavn
         '
         Me.lblNavn.AutoSize = True
-        Me.lblNavn.Location = New System.Drawing.Point(7, 22)
+        Me.lblNavn.Location = New System.Drawing.Point(5, 18)
+        Me.lblNavn.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblNavn.Name = "lblNavn"
-        Me.lblNavn.Size = New System.Drawing.Size(89, 17)
+        Me.lblNavn.Size = New System.Drawing.Size(68, 13)
         Me.lblNavn.TabIndex = 0
         Me.lblNavn.Text = "Velkommen, "
         '
         'btnVis_innlevering
         '
-        Me.btnVis_innlevering.Location = New System.Drawing.Point(6, 20)
-        Me.btnVis_innlevering.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnVis_innlevering.Location = New System.Drawing.Point(4, 16)
+        Me.btnVis_innlevering.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnVis_innlevering.Name = "btnVis_innlevering"
-        Me.btnVis_innlevering.Size = New System.Drawing.Size(143, 49)
+        Me.btnVis_innlevering.Size = New System.Drawing.Size(107, 40)
         Me.btnVis_innlevering.TabIndex = 1
         Me.btnVis_innlevering.Text = "Innlevering"
         Me.btnVis_innlevering.UseVisualStyleBackColor = True
         '
         'btnBytt_Passord
         '
-        Me.btnBytt_Passord.Location = New System.Drawing.Point(6, 20)
-        Me.btnBytt_Passord.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnBytt_Passord.Location = New System.Drawing.Point(4, 16)
+        Me.btnBytt_Passord.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnBytt_Passord.Name = "btnBytt_Passord"
-        Me.btnBytt_Passord.Size = New System.Drawing.Size(143, 49)
+        Me.btnBytt_Passord.Size = New System.Drawing.Size(107, 40)
         Me.btnBytt_Passord.TabIndex = 7
         Me.btnBytt_Passord.Text = "Endre Passord"
         Me.btnBytt_Passord.UseVisualStyleBackColor = True
         '
         'btnLogg_ut
         '
-        Me.btnLogg_ut.Location = New System.Drawing.Point(155, 20)
-        Me.btnLogg_ut.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnLogg_ut.Location = New System.Drawing.Point(116, 16)
+        Me.btnLogg_ut.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnLogg_ut.Name = "btnLogg_ut"
-        Me.btnLogg_ut.Size = New System.Drawing.Size(143, 49)
+        Me.btnLogg_ut.Size = New System.Drawing.Size(107, 40)
         Me.btnLogg_ut.TabIndex = 8
         Me.btnLogg_ut.Text = "Logg ut"
         Me.btnLogg_ut.UseVisualStyleBackColor = True
         '
         'btnStatistikk
         '
-        Me.btnStatistikk.Location = New System.Drawing.Point(6, 73)
-        Me.btnStatistikk.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnStatistikk.Location = New System.Drawing.Point(4, 59)
+        Me.btnStatistikk.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnStatistikk.Name = "btnStatistikk"
-        Me.btnStatistikk.Size = New System.Drawing.Size(143, 49)
+        Me.btnStatistikk.Size = New System.Drawing.Size(107, 40)
         Me.btnStatistikk.TabIndex = 9
         Me.btnStatistikk.Text = "Statistikk"
         Me.btnStatistikk.UseVisualStyleBackColor = True
         '
         'btnUtstyr
         '
-        Me.btnUtstyr.Location = New System.Drawing.Point(155, 20)
-        Me.btnUtstyr.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnUtstyr.Location = New System.Drawing.Point(116, 16)
+        Me.btnUtstyr.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnUtstyr.Name = "btnUtstyr"
-        Me.btnUtstyr.Size = New System.Drawing.Size(143, 48)
+        Me.btnUtstyr.Size = New System.Drawing.Size(107, 39)
         Me.btnUtstyr.TabIndex = 10
         Me.btnUtstyr.Text = "Utstyr"
         Me.btnUtstyr.UseVisualStyleBackColor = True
@@ -196,9 +197,11 @@ Partial Class hoved
         Me.grpOrdre.Controls.Add(Me.btnVis_innlevering)
         Me.grpOrdre.Controls.Add(Me.btnVis_Utleie)
         Me.grpOrdre.Controls.Add(Me.btnVis_Kunder)
-        Me.grpOrdre.Location = New System.Drawing.Point(13, 74)
+        Me.grpOrdre.Location = New System.Drawing.Point(10, 60)
+        Me.grpOrdre.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.grpOrdre.Name = "grpOrdre"
-        Me.grpOrdre.Size = New System.Drawing.Size(307, 128)
+        Me.grpOrdre.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.grpOrdre.Size = New System.Drawing.Size(230, 104)
         Me.grpOrdre.TabIndex = 11
         Me.grpOrdre.TabStop = False
         Me.grpOrdre.Text = "Ordre"
@@ -209,9 +212,11 @@ Partial Class hoved
         Me.grpVirksomhet.Controls.Add(Me.btnVis_Lokasjoner)
         Me.grpVirksomhet.Controls.Add(Me.btnStatistikk)
         Me.grpVirksomhet.Enabled = False
-        Me.grpVirksomhet.Location = New System.Drawing.Point(13, 202)
+        Me.grpVirksomhet.Location = New System.Drawing.Point(10, 164)
+        Me.grpVirksomhet.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.grpVirksomhet.Name = "grpVirksomhet"
-        Me.grpVirksomhet.Size = New System.Drawing.Size(307, 131)
+        Me.grpVirksomhet.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.grpVirksomhet.Size = New System.Drawing.Size(230, 106)
         Me.grpVirksomhet.TabIndex = 12
         Me.grpVirksomhet.TabStop = False
         Me.grpVirksomhet.Text = "Virksomhet"
@@ -220,29 +225,33 @@ Partial Class hoved
         '
         Me.grpSykkler.Controls.Add(Me.btnVis_Sykler)
         Me.grpSykkler.Controls.Add(Me.btnUtstyr)
-        Me.grpSykkler.Location = New System.Drawing.Point(327, 75)
+        Me.grpSykkler.Location = New System.Drawing.Point(245, 61)
+        Me.grpSykkler.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.grpSykkler.Name = "grpSykkler"
-        Me.grpSykkler.Size = New System.Drawing.Size(305, 127)
+        Me.grpSykkler.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.grpSykkler.Size = New System.Drawing.Size(229, 103)
         Me.grpSykkler.TabIndex = 13
         Me.grpSykkler.TabStop = False
-        Me.grpSykkler.Text = "Sykkler"
+        Me.grpSykkler.Text = "Sykler"
         '
         'grpBruker
         '
         Me.grpBruker.Controls.Add(Me.btnBytt_Passord)
         Me.grpBruker.Controls.Add(Me.btnLogg_ut)
-        Me.grpBruker.Location = New System.Drawing.Point(327, 202)
+        Me.grpBruker.Location = New System.Drawing.Point(245, 164)
+        Me.grpBruker.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.grpBruker.Name = "grpBruker"
-        Me.grpBruker.Size = New System.Drawing.Size(305, 131)
+        Me.grpBruker.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.grpBruker.Size = New System.Drawing.Size(229, 106)
         Me.grpBruker.TabIndex = 14
         Me.grpBruker.TabStop = False
         Me.grpBruker.Text = "Bruker"
         '
         'hoved
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(640, 339)
+        Me.ClientSize = New System.Drawing.Size(480, 275)
         Me.Controls.Add(Me.grpBruker)
         Me.Controls.Add(Me.grpSykkler)
         Me.Controls.Add(Me.grpVirksomhet)
@@ -250,7 +259,7 @@ Partial Class hoved
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.GroupBox2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "hoved"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Sykkelmaster"
