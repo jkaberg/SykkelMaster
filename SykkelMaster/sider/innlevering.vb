a76d90bd15eeb79656bd418a0b0ad38ac5e5a348
Potteplanten <bi-z@hotmail.com>
2015-04-18 21:43:04 +0200

    Kommentarer til awesome innlevering

--- SykkelMaster/sider/innlevering.vb
+++ SykkelMaster/sider/innlevering.vb
@@ -1,5 +1,9 @@
 ﻿Public Class innlevering
     Private payload As DataTable
+    ''' <summary>
+    ''' Henter inn lokasjoner til combobox
+    ''' Navngir kolonner i gridview
+    ''' </summary>
     Private Sub innlevering_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         avtaleInnehold()
 
@@ -10,16 +14,21 @@
         End With
     End Sub
 
+    ''' <summary>
+    ''' Sørger for at det ikke søkes etter kunde hvis tekstboksen blir blank
+    ''' </summary>
     Private Sub txtSokKunde_TextChanged(sender As Object, e As EventArgs) Handles txtSokKunde.TextChanged
         If txtSokKunde.Text = "" Then
-            avtaleInnehold() 'Viser alle leie avtaler om man ikke søkt etter kunde
+            avtaleInnehold()
         Else
             sokKunde(txtSokKunde.Text)
         End If
     End Sub
 
+    ''' <summary>
+    ''' Henter ordre til kunden som blir valgt i combobox
+    ''' </summary>
     Private Sub cbxKunde_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxKunde.SelectedIndexChanged
-        'Henter ordrene til kunden
         Dim kunde_id As String = cbxKunde.SelectedValue
 
         If kunde_id > 0 Then
@@ -33,8 +42,10 @@
         End If
     End Sub
 
+    ''' <summary>
+    ''' Endrer status i databasen for sykkelen til innlevert til den stasjonen som er satt i comboboxen
+    ''' </summary>
     Private Sub Avslutt_leie(sender As Object, e As EventArgs) Handles AvsluttLeie.Click
-        'Avslutter leien
         Dim ordre_nr As Integer = daoDelt.finnDGWVerdi(oversiktGrid, "ordre_nr")
 
         If lokasjoner.SelectedValue <> 0 Then
@@ -53,21 +64,30 @@
         End If
     End Sub
 
+    ''' <summary>
+    ''' Henter frem tilbehør når ordren blir valgt
+    ''' </summary>
     Private Sub oversiktGrid_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles oversiktGrid.CellClick
-        'Henter frem tilbehør
+
         With tilbehorGrid
             .DataSource = daoInnlevering.hentTilbehor(daoDelt.finnDGWVerdi(oversiktGrid, "ordre_nr"))
             .Columns("ordre_nr").HeaderText = "Ordrenummer"
             .Columns("navn").HeaderText = "Navn"
         End With
     End Sub
-
+    ''' <summary>
+    ''' Velger hvilken ordre som skal redigeres/innleveres
+    ''' </summary>
     Private Sub cbxLeieAvtaler_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles cbxLeieAvtaler.SelectedIndexChanged
         avtaleInnehold(cbxLeieAvtaler.SelectedValue)
     End Sub
 
+    ''' <summary>
+    ''' Legger kunde i combobox
+    ''' </summary>
+    ''' <param name="sok"> Søker frem kunde </param>
     Private Sub sokKunde(ByVal sok As String)
-        'Søker etter kunde
+
         payload = daoDelt.finnKunde(sok)
 
         payload.Columns.Add("kunde_navn", Type.GetType("System.String"), "fornavn + ' ' + etternavn")
@@ -85,6 +105,10 @@
         End If
     End Sub
 
+    ''' <summary>
+    ''' Sjekk på at det må skrives noe i kundesøk for at databasen skal begynne søk
+    ''' Endrer navn på kolonner for bedre brukervennlighet
+    ''' </summary>
     Private Sub avtaleInnehold(Optional ByVal id As Integer = Nothing)
         If id = Nothing Then
             cbxKunde.DataSource = Nothing
@@ -94,7 +118,6 @@
 
         With Me.oversiktGrid
             .DataSource = daoInnlevering.hentAvtaleInnehold(id)
-            'Endrer navn på headere for å gi en bedre visuell opplevelse
             .Columns("ordre_nr").HeaderText = "Ordrenummer"
             .Columns("frist").HeaderText = "Frist"
             .Columns("sykkeltype").HeaderText = "Sykkeltype"
@@ -106,13 +129,18 @@
         End With
     End Sub
 
+    ''' <summary>
+    ''' Henter opp form person for å kunne redigere kunden som er valgt i tekstboksen
+    ''' </summary>
     Private Sub Rediger_kunde_Click(sender As Object, e As EventArgs) Handles Rediger_kunde.Click
         person.Show()
         person.oppdaterGridView(id:=cbxKunde.SelectedValue)
     End Sub
 
+    ''' <summary>
+    ''' Viser ordrene for med de valgte statusene fra combobox
+    ''' </summary>
     Private Sub cbxStatus_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxStatus.SelectedIndexChanged
-        'viser status for utleien
         Select Case cbxStatus.Text
             Case "Leid ut"
                 avtaleInnehold()
@@ -123,11 +151,12 @@
         End Select
     End Sub
 
+    ''' <summary>
+    ''' Søker etter ordrene der fristen har gått ut
+    ''' </summary>
     Public Sub fristGattUt(ByVal sok As String)
-        'Får opp de som ikke har levert inn sykkelen innen fristen
         With Me.oversiktGrid
             .DataSource = daoInnlevering.hentLeieFrister(sok)
-            'Endre navn for å gi en bedre visuell opplevelse
             .Columns("ordre_nr").HeaderText = "Ordrenummer"
             .Columns("frist").HeaderText = "Frist"
             .Columns("sykkeltype").HeaderText = "Sykkeltype"
