df7676020a880dfe4ca162ba49d3eed27ea07d61
Joel Kåberg <joel@jkaberg.com>
2015-04-18 21:38:12 +0200

    utleie - regn ut korrekt pris

--- SykkelMaster/verktoy/regnPris.vb
+++ SykkelMaster/verktoy/regnPris.vb
@@ -1,10 +1,11 @@
 ﻿Public Class regnPris
+    Private Shared fortjeneste As Integer = 50
 
     Public Shared Function dag(ByVal innpris As Integer)
-        Return innpris * 0.15
+        Return innpris * 0.08 + (5 * fortjeneste)
     End Function
 
     Public Shared Function time(ByVal innpris As Integer)
-        Return innpris * 0.0015
+        Return dag(innpris) * 0.1 + (2 * fortjeneste)
     End Function
 End Class
