acadff2e0813de8b5edaa8d9b36dc4fe39c0059e
Joel Kåberg <joel@jkaberg.com>
2015-04-20 09:38:05 +0200

    clsAnsatt - kommentere kode

--- SykkelMaster/cls/clsAnsatt.vb
+++ SykkelMaster/cls/clsAnsatt.vb
@@ -5,8 +5,10 @@
     Private passord As String
     Private arbeidssted As Integer
     Private tilgangsniva As Integer
-
-    'start
+    ''' <summary>
+    ''' konstruktør for å lag objektet clsPerson, brukes når man logger inn i sykkelmaster
+    ''' </summary>
+    ''' <remarks>tar 7 paramterer, hvert å merk seg ID som brukes for å "finn" noen i databasen</remarks>
     Sub New(ByVal id As Integer,
             ByVal fnavn As String,
             ByVal enavn As String,
@@ -20,8 +22,21 @@
         Me.pArbeidssted = arbeidssted
         Me.pPassord = passord
     End Sub
-
-    'oppdater
+    ''' <summary>
+    ''' konstruktør for å lag objektet clsPerson, brukes for å oppdatere en ansatt
+    ''' </summary>
+    ''' <param name="id">integer</param>
+    ''' <param name="fnavn">string</param>
+    ''' <param name="enavn">string</param>
+    ''' <param name="pnr">object</param>
+    ''' <param name="tnr">object</param>
+    ''' <param name="adresse">string</param>
+    ''' <param name="epost">string</param>
+    ''' <param name="sted">string</param>
+    ''' <param name="stilling">integer</param>
+    ''' <param name="provisjon">integer</param>
+    ''' <param name="arbeidssted">integer</param>
+    ''' <remarks>tar 11 paramterer, hvert å merk seg ID som brukes for å "finn" noen i databasen</remarks>
     Sub New(ByVal id As Integer,
             ByVal fnavn As String,
             ByVal enavn As String,
@@ -39,8 +54,20 @@
         Me.pProvisjon = provisjon
         Me.pArbeidssted = arbeidssted
     End Sub
-
-    'legg til
+    ''' <summary>
+    ''' konstruktør for å lag objektet clsPerson, brukes for å legg til en ansatt i databasen
+    ''' </summary>
+    ''' <param name="fnavn">string</param>
+    ''' <param name="enavn">string</param>
+    ''' <param name="pnr">string</param>
+    ''' <param name="tnr">object</param>
+    ''' <param name="adresse">string</param>
+    ''' <param name="epost">string</param>
+    ''' <param name="stilling">integer</param>
+    ''' <param name="provisjon">integer</param>
+    ''' <param name="arbeidssted">integer</param>
+    ''' <param name="passord">string</param>
+    ''' <remarks>tar 10 parametrer</remarks>
     Sub New(ByVal fnavn As String,
             ByVal enavn As String,
             ByVal pnr As String,
@@ -59,11 +86,22 @@
         Me.pArbeidssted = arbeidssted
     End Sub
 
-    'fjern
+    ''' <summary>
+    ''' konstruktør for å lag objektet clsPerson, brukes for å fjern til en ansatt i databasen
+    ''' </summary>
+    ''' <param name="id">integer</param>
+    ''' <param name="fnavn">string</param>
+    ''' <param name="enavn">string</param>
+    ''' <remarks>tar 3 paramterer</remarks>
     Sub New(ByVal id As Integer, ByVal fnavn As String, ByVal enavn As String)
         MyBase.New(id, fnavn, enavn)
     End Sub
-
+    ''' <summary>
+    ''' egenskapen pStilling
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Public Property pStilling() As Integer
         Get
             Return stilling
@@ -76,7 +114,10 @@
             End If
         End Set
     End Property
-
+    ''' <summary>
+    ''' henter eller setter provisjon, sjekker om veriden er numerisk
+    ''' </summary>
+    ''' <returns>variablen provisjon som integer</returns>
     Public Property pProvisjon() As Integer
         Get
             Return provisjon
@@ -89,7 +130,10 @@
             End If
         End Set
     End Property
-
+    ''' <summary>
+    ''' henter eller setter provisjon, sjekker om lengden på veriden er mindre enn 3 tegn
+    ''' </summary>
+    ''' <returns>variablen passord som streng</returns>
     Public Property pPassord() As String
         Get
             Return passord
@@ -103,7 +147,10 @@
 
         End Set
     End Property
-
+    ''' <summary>
+    ''' henter eller setter arbeidssted, sjekker at veriden ikke er 0
+    ''' </summary>
+    ''' <returns>variablen arbeidssted</returns>
     Public Property pArbeidssted() As Integer
         Get
             Return arbeidssted
